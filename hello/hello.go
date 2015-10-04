08ab891c53c49fc14b3939cbe92a3ef95335c518
dev <dev@Chris-Mallons-MacBook-Pro.local>
2015-10-04 21:57:23 +0100

    initial commit of go tutorials

---
+++ hello/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/JaegerBane/stringutil"
+)
+
+func main() {
+    fmt.Printf("alreet\n")
+
+    fmt.Printf(stringutil.Reverse("!oG ,olleH") + "\n")
+}

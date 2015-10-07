ff2de2c60e3ae52599abf0054e7efbdccf29019a
dev <dev@Chris-Mallons-MacBook-Pro.local>
2015-10-07 22:41:24 +0100

    messing around with consts

--- hello/hello.go
+++ hello/hello.go
@@ -7,9 +7,31 @@ import (
 )
 
 func main() {
+
+    /* Main is full of random junk at the moment */
+
+    const COMP int = 10
+
+    var stuff int = COMP
+
+    /* const declaration of type int */
+
     fmt.Printf("alreet\n")
+
+    /* Printf only takes strings initially - %T shortcut for variable type? */
+
+    fmt.Printf("stuff is of type %T\n",stuff)
+
+    /* Println prints anything - printing an int here */
+
+    fmt.Println(stuff)
+
+    /* stuff from tutorial */
 
     fmt.Printf(stringutil.Reverse("!oG ,olleH") + "\n")
 
     fmt.Printf(stringutil.Reverse("Hello go") + "\n")
+
+    
+
 }
